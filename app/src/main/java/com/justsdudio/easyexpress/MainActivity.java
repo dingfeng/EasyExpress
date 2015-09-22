a1fc5ac252f1eb9d49d77dbf7be00d9e7b37fb0a
dingfeng <1041460265@qq.com>
2015-09-22 21:48:32 +0800

    网络连接

--- app/src/main/java/com/justsdudio/easyexpress/MainActivity.java
+++ app/src/main/java/com/justsdudio/easyexpress/MainActivity.java
@@ -1,6 +1,9 @@
 package com.justsdudio.easyexpress;
 
+import android.annotation.TargetApi;
 import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.Intent;
 import android.content.res.AssetManager;
 import android.content.res.Resources;
@@ -8,7 +11,11 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.drawable.Drawable;
 import android.media.Image;
+import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
+import android.service.dreams.DreamService;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -16,17 +23,30 @@ import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import org.w3c.dom.Text;
+
+import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 
+import ExpressHttpRequest.DataRequest;
+import ExpressHttpRequest.ExpressInfo;
 
 
 public class MainActivity extends Activity {
     private static  Map<String,String> expressMap;
     private ImageView logoView;
     private TextView expressNameView;
+    private DataRequest dataRequest;
+    private TextView idView;
+    private TextView idShowView;
+    private TextView stateView;
     private void init()
     {
        /* try {
@@ -43,6 +63,10 @@ public class MainActivity extends Activity {
         } catch (Exception e) {
             e.printStackTrace();
         }*/
+        dataRequest = new DataRequest();
+        idView = (TextView) findViewById(R.id.id_text);
+        idShowView = (TextView) findViewById(R.id.express_num);
+        stateView = (TextView) findViewById(R.id.express_status);
         expressMap = new HashMap<String,String>();
         String[] express_ids = getResources().getStringArray(R.array.express_id);
         String[] express_names = getResources().getStringArray(R.array.date);
@@ -59,6 +83,7 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         init();
 
     }
@@ -101,6 +126,14 @@ public class MainActivity extends Activity {
            Intent intent = new Intent(this, ChooseExpressActivity.class);
            startActivityForResult(intent, 0);
        }
+       //输入订单编号后要求查询
+       else if (target.getId() == R.id.search_button)
+       {
+          String id = this.idView.getText().toString();
+          String expressName = this.expressNameView.getText().toString();
+          RequestTask task = new RequestTask(this);
+           task.execute(expressName,id);
+       }
     }
     @Override
     public void onActivityResult(int requestCode,int resultCode,Intent intent)
@@ -116,17 +149,126 @@ public class MainActivity extends Activity {
         }
     }
 
-    private void setExpressIconByExpressName(String expressName)
-    {
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
+    private void setExpressIconByExpressName(String expressName) {
+        String expressId = expressMap.get(expressName);
         Resources res = this.getResources();
         AssetManager assetManager = res.getAssets();
+        String filename = null;
+        boolean exists = true;
         try {
-            Drawable image = Drawable.createFromStream(assetManager.open("imagepng/"+expressName),expressName);
-            logoView.setBackground(image);
-            //logoView.invalidateDrawable(image);
+            filename = "expressImage/" + expressId + ".png";
+            InputStream in = assetManager.open(filename);
+            Drawable drawable = Drawable.createFromStream(in,expressId);
+            logoView.setBackground(drawable);
         } catch (IOException e) {
+            exists = false;
             e.printStackTrace();
         }
+        if (!exists)
+        {
+            filename = "expressImage/unknown.png";
+            try {
+                InputStream in = assetManager.open(filename);
+                Drawable drawable = Drawable.createFromStream(in,expressId);
+                logoView.setBackground(drawable);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
+
+    class RequestTask extends AsyncTask<String,Integer,ExpressInfo>
+    {
+
+        private  Context context;
+        private ProgressDialog pdialog;
+        public RequestTask(Context context)
+        {
+            this.context = context;
+        }
+
+        //执行中
+        @Override
+        protected ExpressInfo doInBackground(String... params) {
+            String expressName = expressMap.get(params[0]);
+            String id = params[1];
+            Log.v("expressName : ",expressName);
+            Log.v("expressId",id);
+            Log.v("sdf","sdf");
+            ExpressInfo info = null;
+            try {
+              info = dataRequest.findExrpessInfo(expressName, id);
+            }catch (Exception e)
+            {
+                Log.e("error:",e.toString());
+            }
+            return info;
+        }
+
+        //执行之后
+        @Override
+        protected  void onPostExecute(ExpressInfo info)
+        {
+            if (info == null) {
+                pdialog.dismiss();
+                Toast toast = Toast.makeText(context,"查询异常",Toast.LENGTH_SHORT);
+                toast.show();
+                return;
+            }
+            idShowView.setText(info.getId());
+            String state = null;
+            switch (info.getState())
+            {
+                //未知
+                case UNKOWN:
+                    state = "未知";
+                    break;
+                //已签收
+                case RECEIVED:
+                    state = "已签收";
+                    break;
+                //未签收
+                case HASNOTBEENRECEIVED:
+                    state = "未签收";
+                    break;
+            }
+            stateView.setText(state);
+            ArrayList<String> list = new ArrayList<String>();
+            Iterator<String> itr = info.getInfos();
+            while(itr.hasNext())
+            {
+                list.add(itr.next());
+            }
+            if (list.size() != 0)
+            {
+                String[] infos = new String[list.size()];
+                list.toArray(infos);
+                setExpressInfo(infos);
+            }
+            pdialog.dismiss();
+        }
+
+        //执行之前
+        @Override
+        protected  void onPreExecute()
+        {
+           pdialog = new ProgressDialog(context);
+            //进度条为环状进度条
+           pdialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+            //进度条不显示进度
+           pdialog.setIndeterminate(false);
+           pdialog.show();
+        }
+
+        //执行中更新
+        @Override
+        protected  void onProgressUpdate(Integer... value)
+        {
+            //do nothing
+        }
+
     }
 
 
